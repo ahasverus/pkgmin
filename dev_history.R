3f2643dcd62d3f8a2c03d556122a58a8395d6544
Nicolas Casajus <nicolas.casajus@gmail.com>
2019-11-25 15:35:19 +0100

    Add Dev command lines history

---
+++ dev_history.R
@@ -0,0 +1,102 @@
+# git reset --hard HEAD~1
+
+path   <- "/Users/nicolascasajus/Documents"
+pkname <- "pkgmin"
+
+usethis::create_package(file.path(path, pkname))
+
+usethis::use_git()
+
+usethis::edit_file("DESCRIPTION")
+## ● Edit 'DESCRIPTION'
+system("git add -A")
+system("git commit -m 'Edit package metadata'")
+
+usethis::use_mit_license(name = "Nicolas Casajus")
+system("git add -A")
+system("git commit -m 'Add package license'")
+
+usethis::use_roxygen_md()
+system("git add -A")
+system("git commit -m 'Setting roxygen2'")
+
+usethis::use_package_doc()
+system("git add -A")
+system("git commit -m 'Edit package documentation'")
+
+devtools::document()
+system("git add -A")
+system("git commit -m 'Generate package documentation (Rd file)'")
+
+usethis::use_r("moyenne")
+## ● Edit 'R/moyenne.R' (R code)
+system("git add -A")
+system("git commit -m 'Implement R function'")
+
+usethis::use_r("moyenne")
+## ● Edit 'R/moyenne.R' (roxygen)
+system("git add -A")
+system("git commit -m 'Add roxygen documentation'")
+
+devtools::document()
+system("git add -A")
+system("git commit -m 'Generate moyenne documentation (Rd file)'")
+
+usethis::use_package(package = "magrittr")
+usethis::use_package(package = "stats")
+system("git add -A")
+system("git commit -m 'Add external dependencies'")
+
+set.seed(1637)
+x  <- rnorm(9)
+y  <- c(NA, 2:9)
+xy <- data.frame(x, y)
+usethis::use_data(xy, internal = FALSE)
+system("git add -A")
+system("git commit -m 'Add internal dataset'")
+
+usethis::use_r("xy")
+## ● Edit 'R/xy.R' (roxygen)
+system("git add -A")
+system("git commit -m 'Add roxygen documentation'")
+
+devtools::document()
+system("git add -A")
+system("git commit -m 'Generate dataset documentation (Rd file)'")
+
+usethis::use_r("moyenne")
+## ● Edit 'R/moyenne.R' (roxygen)
+system("git add -A")
+system("git commit -m 'Update roxygen example'")
+
+devtools::document()
+system("git add -A")
+system("git commit -m 'Update moyenne documentation (Rd file)'")
+
+devtools::check()
+
+usethis::use_version(which = "major")
+
+usethis::use_vignette("pkgmin")
+system("git add -A")
+system("git commit -m 'Create package vignette'")
+
+## ● Edit 'vignettes/pkgmin.Rmd'
+system("git add -A")
+system("git commit -m 'Edit package vignette'")
+
+usethis::use_readme_rmd()
+system("git add -A")
+system("git commit -m 'Create README' --no-verify")
+
+## ● Edit 'README.Rmd'
+rmarkdown::render("README.Rmd")
+usethis::use_git_ignore("README.html")
+system("git add -A")
+system("git commit -m 'Edit README'")
+
+## Remove inst/doc from .gitignore
+system("git add -A")
+system("git commit -m 'Remove inst/doc'")
+
+usethis::use_github(protocol = "https")
